310553c675cc4f563370876370afee0e90792b8e
mapsteps <connect@mapsteps.com>
2021-04-12 11:07:24 +0200

    initial commit

---
+++ woocommerce-quick-view.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP Video Popup
+ * Plugin URI: https://wp-video-popup.com
+ * Description: Add beautiful responsive YouTube & Vimeo Video lightbox popups to your WordPress website.
+ * Version: 2.9.1
+ * Author: David Vongries
+ * Author URI: https://mapsteps.com
+ *
+ * @package WP_Video_Popup
+ */
+
+defined( 'ABSPATH' ) || die( "Can't access directly" );
